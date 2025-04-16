6d344e0d63c68a1e7e8cb70ef855cf39ee57822f
Andrew Tabala <evilicekinggunter@gmail.com>
2025-04-16 15:28:28 +0300

    Rebuild

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         {/* Public route for login */}
         <Route path="/login" element={<Login />} />
